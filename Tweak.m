e1bd8140427e0c4798c48bb67f59105f92395bef
Kronsten767 <davidct2006@gmail.com>
2026-05-06 19:46:23 +0200

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -2,8 +2,7 @@
 #import <objc/runtime.h>
 #import <UIKit/UIKit.h>
 
-// 1. Declaración manual de MSHookFunction para no necesitar "lo de Cydia" (headers)
-// Esto permite que el dylib use el motor de inyección de Swift aunque sea un .m
+// --- 1. DECLARACIONES PARA INYECCIÓN SWIFT ---[cite: 7]
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,38 +12,41 @@ extern "C" {
 }
 #endif
 
-// --- VECTORES DE RETORNO ESTÁTICO ---[cite: 2]
+// --- 2. VECTORES DE RETORNO ---[cite: 2]
 BOOL return_NO(id self, SEL _cmd) { return NO; }
 long long return_1(id self, SEL _cmd) { return 1; }
 
-// --- BYPASS SWIFT LIVENESS (FOTO) ---
-// Usamos el símbolo exacto del binario Hoop_3[cite: 6]
-bool (*orig_liveness)(void* self);
-bool hook_liveness(void* self) {
-    return false; // Forzamos que NO pida la foto a nivel de CPU
+// ID de hardware falso para evitar rastreo/baneos
+id fake_device_id(id self, SEL _cmd) {
+    return [[NSUUID alloc] initWithUUIDString:@"A1B2C3D4-E5F6-4789-90AB-CDEF12345678"];
 }
 
-// --- MOTOR DE INYECCIÓN ---[cite: 2, 7]
-__attribute__((constructor)) static void apply_hoop_master_bypass() {
+// --- 3. LOGICA DE BYPASS SWIFT ---
+bool (*orig_liveness)(void* self);
+bool hook_liveness(void* self) {
+    return false; // El servidor pide foto -> El Dylib dice NO.
+}
 
-    // A. SWIFT BYPASS (MÉTODO SEGURO)
-    // Buscamos el símbolo que extrajimos de Hoop_3[cite: 6]
-    void* symbol = MSFindSymbol(NULL, "_$s4Hoop11TripManagerC31nextTripRequiresLivenessChallengeSbvg");
-    if (symbol) {
-        // Aunque estemos en un .m, esto inyectará el falso en el núcleo de Swift
-        MSHookFunction(symbol, (void*)hook_liveness, (void**)&orig_liveness);
+// --- 4. MOTOR MAESTRO DE INYECCIÓN ---
+__attribute__((constructor)) static void apply_hoop_complete_bypass() {
+
+    // A. BYPASS FACIAL (MÉTODO DUAL)
+    // Nivel 1: Variable lógica de Swift[cite: 6]
+    void* livenessSym = MSFindSymbol(NULL, "_$s4Hoop11TripManagerC31nextTripRequiresLivenessChallengeSbvg");
+    if (livenessSym) {
+        MSHookFunction(livenessSym, (void*)hook_liveness, (void**)&orig_liveness);
     }
-
-    // B. BYPASS DE INTERFAZ (DISMISS AUTOMÁTICO)
-    // Si la pantalla de verificación logra aparecer, la obligamos a cerrarse sola
+    // Nivel 2: Interfaz de usuario (Auto-dismiss)[cite: 1]
     Class livenessVC = NSClassFromString(@"Hoop.LivenessViewController");
     if (livenessVC) {
-        Method mDidLoad = class_getInstanceMethod(livenessVC, @selector(viewDidAppear:));
-        IMP dismissImp = imp_implementationWithBlock(^(id _self, BOOL animated) {
+        method_setImplementation(class_getInstanceMethod(livenessVC, @selector(viewDidAppear:)), 
+        imp_implementationWithBlock(^(id _self, BOOL animated) {
             [(UIViewController *)_self dismissViewControllerAnimated:YES completion:nil];
-        });
-        method_setImplementation(mDidLoad, dismissImp);
+        }));
     }
+
+    // B. SPOOFING DE HARDWARE (IDENTIFIER FOR VENDOR)
+    method_setImplementation(class_getInstanceMethod([UIDevice class], @selector(identifierForVendor)), (IMP)fake_device_id);
 
     // C. ENMASCARAMIENTO GPS[cite: 2]
     Class locInfo = NSClassFromString(@"CLLocationSourceInformation");
@@ -59,4 +61,10 @@ __attribute__((constructor)) static void apply_hoop_master_bypass() {
         method_setImplementation(class_getInstanceMethod(reach, NSSelectorFromString(@"isReachableViaWiFi")), (IMP)return_NO);
         method_setImplementation(class_getInstanceMethod(reach, NSSelectorFromString(@"currentReachabilityStatus")), (IMP)return_1);
     }
+
+    // E. BYPASS DE DETECCIÓN DE GRABACIÓN / SCREENSHOT
+    method_setImplementation(class_getInstanceMethod([UIScreen class], @selector(isCaptured)), (IMP)return_NO);
+
+    // F. ANTI-TAMPER (OCULTAR RUTA DE SIDELOAD)
+    method_setImplementation(class_getInstanceMethod([NSFileManager class], @selector(fileExistsAtPath:)), (IMP)return_NO);
 }
